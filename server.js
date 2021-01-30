aa19fb65904624a83c6010247d56dbbd9b59382b
Roseanne Nguyen <rtnguye1@gmail.com>
2021-01-30 11:01:16 -0700

    changes tables to table in all html files to link properly

--- server.js
+++ server.js
@@ -1,50 +1,49 @@
 var express = require("express");
-    var path = require("path");
+var path = require("path");
 
-    var app = express();
-    var PORT = process.env.PORT || 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-    // api tables/waitlist
+// api tables/waitlist
 
-    var newReservation = [
-        {
-            customerName: "Josh",
-            phoneNumber: "1234436",
-            customerEmail: "fun@gmail.com",
-            customerID: 1,
-        },
-        {
-            customerName: "Andrew",
-            phoneNumber: "0986748",
-            customerEmail: "fun@gmail.com",
-            customerID: 2,
-        },
-        {
-            customerName: "Skye",
-            phoneNumber: "345678678",
-            customerEmail: "fun@gmail.com",
-            customerID: 3,
-        }
-    ];
-
-
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "index.html"));
-      });
-      
-      app.get("/reserve", function(req, res) {
-        res.sendFile(path.join(__dirname, "reserve.html"));
-      });
-
-      app.get("/table", function(req, res) {
-        res.sendFile(path.join(__dirname, "table.html"));
-      });
+var newReservation = [
+  {
+    customerName: "Josh",
+    phoneNumber: "1234436",
+    customerEmail: "fun@gmail.com",
+    customerID: 1,
+  },
+  {
+    customerName: "Andrew",
+    phoneNumber: "0986748",
+    customerEmail: "fun@gmail.com",
+    customerID: 2,
+  },
+  {
+    customerName: "Skye",
+    phoneNumber: "345678678",
+    customerEmail: "fun@gmail.com",
+    customerID: 3,
+  }
+];
 
 
-      app.listen(PORT, function() {
-        console.log("App listening on PORT " + PORT);
-      });
-      
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/table", function (req, res) {
+  res.sendFile(path.join(__dirname, "table.html"));
+});
+
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
