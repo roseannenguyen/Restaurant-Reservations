018fc7bc39ece28c99ed8c9619b23a1a34bf0ae7
Roseanne Nguyen <rtnguye1@gmail.com>
2021-01-30 14:27:10 -0700

    edited updates if the if/else statement on line 49-60, but still appending incorrectly to both waitlist and table api

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const OUTPUT_DIR = path.resolve(__dirname, "api");
-const outputPath = path.join(OUTPUT_DIR, "table.json");
-
 const reservation = [];
 const waitlist = [];
 let people = 0;
@@ -32,6 +29,8 @@ app.get("/table", function (req, res) {
   res.sendFile(path.join(__dirname, "table.html"));
 });
 
+
+
 app.get("/api/waitlist", function (req, res) {
   return res.json(waitlist);
 });
@@ -47,24 +46,18 @@ app.post("/api/table", function (req, res) {
 
   var data = req.body
 
-  let results = reservation.push(data)
-  console.log(reservation.length)
-  
   if (people.length < 5) {
 
-    fs.writeFileSync(outputPath, JSON.stringify(results), (err) => {
-      if (err) throw err;
-      console.log("You are booked!");
+    reservation.push(data)
+    res.json(true)
 
-
-    })
   }
 
   else {
     waitlist.push(data)
+    res.json(false)
+
   }
-
-
 
 });
 
@@ -74,6 +67,8 @@ app.post("/api/clear", function (req, res) {
   reservation.length = [];
   waitlist.length = [];
   people.length = 0;
+
+  res.json({ ok: true })
 });
 
 
